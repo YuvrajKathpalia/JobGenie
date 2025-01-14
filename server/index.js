0623e5bbb9e0dbf4dffde8a75b31256d294c05b2
YuvrajKathpalia <yuvraj.kathpalia20@gmail.com>
2025-01-14 23:00:19 +0530

    minor changes

--- server/index.js
+++ server/index.js
@@ -7,7 +7,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+//app.use(cors());
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://biotech-trek.vercel.app/'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 const uri = process.env.MONGO_URI;
