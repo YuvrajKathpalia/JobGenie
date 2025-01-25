f3994dc02a893263d128ce9bb431a487ceb2e2fb
YuvrajKathpalia <yuvraj.kathpalia20@gmail.com>
2025-01-26 01:25:42 +0530

    n

--- server/index.js
+++ server/index.js
@@ -8,11 +8,11 @@ dotenv.config();
 const app = express();
 
 
-// app.use(cors({
-//   origin: 'http://localhost:5174',  // Frontend's URL
-//   methods: 'GET,POST,PUT,DELETE',
-//   allowedHeaders: 'Content-Type,Authorization'
-// }));
+app.use(cors({
+  origin: 'http://localhost:5173',  
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
 
 app.options('*', cors());  // This handles all OPTIONS requests
 
